b24a1c1af88fab59ea986d7f92a4ceae79c8769e
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-08 14:44:18 +0100

    Do template basic

--- index.php
+++ index.php
@@ -7,7 +7,30 @@
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
     <title>PHP Strong Password Generator</title>
 </head>
-<body>
-    
+<body class="bg-dark">
+    <header>
+        <div class="container text-center mt-5">
+            <h1 class="text-primary">Strong Password Generator</h1>
+            <h2 class="text-light">Genera una password sicura</h3>
+        </div>
+    </header>
+    <main>
+        <section id="password" class="mt-5">
+        <div class="container">
+            <p class="py-5 ps-2 text-light bg-info rounded h3">Nessun parametro valido inserito</p>
+            <form action="#" method="post" class=" bg-danger">
+                <div class="d-flex justify-content-between p-3 mb-5">
+                    <label for="length" class="text-light">Lunghezza password</label>
+                    <input type="text" name="length" id="length">
+                </div>
+                <div class="p-3">
+                    <button name="send" class="btn btn-primary">Invia</button>
+                    <button name="cancel" class="btn btn-secondary">Annulla</button>
+                </div>
+            </form>
+        </div>
+
+        </section>
+    </main>
 </body>
 </html>
